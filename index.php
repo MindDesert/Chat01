c6b7412f782fee332a6e8631e1e24165f3ae8312
MindDesert <83793323+MindDesert@users.noreply.github.com>
2021-05-12 04:09:20 +0200

    nice

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-    require 'vendor/autoload.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
     // create an instance
     $mail= new PHPMailer();
     
